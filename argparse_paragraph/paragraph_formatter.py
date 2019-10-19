ae7486e75a6b94423d68687d4443673dc0300530
David Steele <dsteele@gmail.com>
2019-10-19 15:06:04 -0400

    Support line separators containing white space

--- argparse_paragraph/paragraph_formatter.py
+++ argparse_paragraph/paragraph_formatter.py
@@ -5,16 +5,18 @@ import textwrap
 class ParagraphFormatter(HelpFormatter):
     """
     An argparse formatter that preserves paragraphs
-    (text separated by "\n\n")
+    (text separated by a blank line)
     """
 
     def __init__(self, *args, **kwargs):
         self.psep = "\n\n"
+        self.psepre = "\n\\s*\n"
         super().__init__(*args, **kwargs)
 
     def _fill_text(self, text, width, indent):
         formatted = []
-        for paragraph in text.split(self.psep):
+        #for paragraph in text.split(self.psep):
+        for paragraph in re.split(self.psepre, text):
             paragraph = self._whitespace_matcher.sub(" ", paragraph).strip()
             formatted.append(
                 textwrap.fill(
@@ -29,7 +31,7 @@ class ParagraphFormatter(HelpFormatter):
 
     def _split_lines(self, text, width):
         formatted = []
-        for paragraph in text.split(self.psep):
+        for paragraph in re.split(self.psepre, text):
             paragraph = self._whitespace_matcher.sub(" ", paragraph).strip()
             if formatted:
                 formatted.append("")
