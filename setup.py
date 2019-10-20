5fbe9982b619926099cccb4f7cbe9025ed6a9385
David Steele <dsteele@gmail.com>
2019-10-20 00:42:40 -0400

    Prep setup.py for PyPi release

--- setup.py
+++ setup.py
@@ -4,12 +4,37 @@
 # License-Filename: LICENSE
 #
 
+from os import path
 from setuptools import setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="argparse_paragraph",
     version="1.0",
     packages=["argparse_paragraph"],
+    url="https://github.com/davesteele/argparse_formatter",
+    license="GPL 2.0",
+    author="David Steele",
+    author_email="dsteele@gmail.com",
+    description="Paragraph-preserving formatter for argparse",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Environment :: Console",
+        "License :: OSI Approved :: "
+        "GNU General Public License v2 or later (GPLv2+)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Software Development :: User Interfaces",
+    ]
 )
