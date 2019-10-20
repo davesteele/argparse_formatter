3b3595980781c0b2c2c6e8f9bfabcf42bad19e02
David Steele <dsteele@gmail.com>
2019-10-20 00:16:07 -0400

    Add Copyright declarations

--- argparse_paragraph/paragraph_formatter.py
+++ argparse_paragraph/paragraph_formatter.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2019 David Steele <dsteele@gmail.com>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+# License-Filename: LICENSE
+#
+
 from argparse import HelpFormatter
 import re
 import textwrap
--- test/test_phelp.py
+++ test/test_phelp.py
@@ -1,3 +1,9 @@
+# Copyright (c) 2019 David Steele <dsteele@gmail.com>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+# License-Filename: LICENSE
+#
+
 import argparse
 from collections import namedtuple
 from pathlib import Path
